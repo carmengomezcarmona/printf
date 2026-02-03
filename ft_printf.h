df8cb4362386d3a70b438aaee97281fff7434aa2
Carmen Cecilia Gomez Carmona <carmgome@c3r7s6.42malaga.com>
2026-02-03 11:13:53 +0100

    Printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,15 +6,15 @@
 /*   By: carmgome <carmgome@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/18 19:06:55 by carmgome          #+#    #+#             */
-/*   Updated: 2026/01/19 11:20:07 by carmgome         ###   ########.fr       */
+/*   Updated: 2026/02/03 10:34:50 by carmgome         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H //si no esta definida la etiqueta ft_printf_h...
-# define FT_PRINTF_H //definela ahora mismo
+#ifndef FT_PRINTF_H 
+# define FT_PRINTF_H 
 
-# include <unistd.h> // La herramienta para escribir (write)
-# include <stdarg.h> // La herramienta de los regalos mágicos
+# include <unistd.h> 
+# include <stdarg.h> 
 
 int	ft_putchar_printf(char c);
 int	ft_putstr_printf(char *s);
@@ -22,8 +22,7 @@ int	ft_putnbr_printf(int n);
 int	ft_putunsigned_base(unsigned int n, char *base, int base_len);
 int	ft_putptr_printf(unsigned long n);
 
-// Prototipos de las funciones principales
 int	ft_selector(va_list args, char format);
 int	ft_printf(char const *str, ...);
 
-#endif //cierra el bloque 
+#endif 
--- main.c
+++
@@ -1,7 +0,0 @@
-#include "ft_printf.h"
-
-int main(void)
-{
-    ft_printf("Hola %s %d %x %%\n", "42", 2025, 255);
-    return (0);
-}
